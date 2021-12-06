10d6fe0d776043774cedde51706682848e27bfa5
lunnlew <4288315+lunnlew@users.noreply.github.com>
2021-12-06 16:05:18 +0800

    fx

--- src/backend/routes/system.ts
+++ src/backend/routes/system.ts
@@ -28,40 +28,80 @@ const ServiceInstall = async (req, res, next) => {
 
     // Listen for the "install" event, which indicates the
     // process is available as a service.
-    svc.on('install', function () {
-        console.log('Service install');
-        svc.start();
+    svc.once('install', function (data) {
+        if (data.state == 'success') {
+            console.log('Service installed');
+            svc.start();
+        } else {
+            console.log('Service install error');
+            res.end('Service install error')
+        }
     });
-    svc.on('start', function () {
-        console.log('Service started');
+    svc.once('start', function (data) {
+        if (data.state == 'success') {
+            console.log('Service started');
+            res.end('Service started')
+        } else {
+            console.log('Service start error');
+            res.end('Service start error')
+        }
     });
-    svc.on('error', function () {
+    svc.once('error', function () {
         console.log('Service error');
+        res.end('Service error')
     });
 
     svc.install();
-    res.end("windows service is installing")
 }
 
 
 const ServiceUninstall = async (req, res, next) => {
     // Listen for the "uninstall" event
-    svc.on('stop', function () {
-        console.log('Service stopped');
-        svc.uninstall()
+    svc.once('stop', function (data) {
+        if (data.state == 'success') {
+            console.log('Service stopped');
+            svc.uninstall();
+        } else {
+            console.log('Service stop error')
+            res.end('Service stop error')
+        }
     });
-    svc.on('uninstall', function () {
-        console.log('Service uninstall');
+    svc.once('uninstall', function (data) {
+        if (data.state == 'success') {
+            console.log('Service uninstalled');
+            res.end('Service uninstalled')
+        } else {
+            console.log('Service uninstall error');
+            res.end('Service uninstall error')
+        }
     })
-    svc.on('error', function () {
+    svc.once('error', function () {
         console.log('Service error');
+        res.end('Service error')
     });
 
     svc.stop();
-    res.end("windows service is uninstalling")
+}
+
+const ServiceStatus = async (req, res, next) => {
+    svc.once('status', function (data) {
+        if (data.state == 'success') {
+            console.log('Service status: ' + data.data);
+            res.end(data.data)
+        } else {
+            console.log('Service status error');
+            res.end('Service status error')
+        }
+    })
+    svc.once('error', function (data) {
+        console.log('Service error', data);
+        res.end('Service error')
+    });
+    svc.status()
 }
 
 router.get("/installAsService", ServiceInstall)
+router.get("/serviceStatus", ServiceStatus)
 router.get("/uninstallService", ServiceUninstall)
 
 export default router
