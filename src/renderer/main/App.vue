1c7553eb8dbfab4854ce34121648cb23a80eebde
lunnlew <4288315+lunnlew@users.noreply.github.com>
2021-12-08 21:00:27 +0800

    更新弹窗界面样式

--- src/renderer/main/App.vue
+++ src/renderer/main/App.vue
@@ -4,11 +4,11 @@
 
 <script>
 export default {
-  name: "App"
+  name: "App",
 };
 </script>
 
-<style>
+<style lang="less">
 *,
 html {
   padding: 0;
@@ -29,5 +29,24 @@ body,
   background-color: #5c646c;
   box-sizing: border-box;
   padding: 10px 20px;
+}
+.el-dialog {
+  background: #434343 !important;
+  color: #fff;
+  font-size: 14px;
+  .el-dialog__title {
+    color: #fff;
+    font-size: 16px;
+  }
+  .el-form-item__label {
+    color: #fff;
+  }
+  .el-dialog__headerbtn{
+    top: 15px;
+  }
+  .el-dialog__header {
+    background: #383838;
+    padding: 10px;
+  }
 }
 </style>
--- src/renderer/main/components/dialog/newLib.vue
+++ src/renderer/main/components/dialog/newLib.vue
@@ -1,74 +1,81 @@
 <template>
-    <el-dialog v-model="dialogVisible" title="增加资源库" @close="$emit('hide')">
-        <el-form ref="form" :model="libform" label-width="120px">
-            <el-form-item label="资源库名称">
-                <el-input v-model="libform.name"></el-input>
-            </el-form-item>
-            <el-form-item label="目标文件夹">
-                <el-input :disabled="true" v-model="libform.path" placeholder="请选择文件夹">
-                    <template #append>
-                        <el-button type="primary" @click="onSelectDirectory">选择</el-button>
-                    </template>
-                </el-input>
-            </el-form-item>
-            <el-form-item label="资源库类型">
-                <el-select v-model="libform.type" placeholder="请选择资源库类型">
-                    <el-option label="电影" value="movie"></el-option>
-                    <!-- <el-option label="电视剧" value="tv"></el-option> -->
-                </el-select>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="onSubmit">保存</el-button>
-                <el-button @click="$emit('hide')">取消</el-button>
-            </el-form-item>
-        </el-form>
-    </el-dialog>
+  <el-dialog v-model="dialogVisible" title="增加资源库" @close="$emit('hide')">
+    <el-form ref="form" :model="libform" label-width="100px">
+      <el-form-item label="资源库名称">
+        <el-input size="medium" v-model="libform.name"></el-input>
+      </el-form-item>
+      <el-form-item label="目标文件夹">
+        <el-input
+          size="medium"
+          :disabled="true"
+          v-model="libform.path"
+          placeholder="请选择文件夹"
+        >
+          <template #append>
+            <el-button type="primary" @click="onSelectDirectory"
+              >选择</el-button
+            >
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item label="资源库类型">
+        <el-select
+          size="medium"
+          v-model="libform.type"
+          placeholder="请选择资源库类型"
+        >
+          <el-option label="电影" value="movie"></el-option>
+          <!-- <el-option label="电视剧" value="tv"></el-option> -->
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">保存</el-button>
+        <el-button @click="$emit('hide')">取消</el-button>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
 </template>
 <script lang="ts">
-import {
-    defineComponent,
-    reactive,
-    ref
-} from "vue";
-import { useStore } from 'vuex';
-import { libMenuClick } from '@/lib/sideMenu';
+import { defineComponent, reactive, ref } from "vue";
+import { useStore } from "vuex";
+import { libMenuClick } from "@/lib/sideMenu";
 export default defineComponent({
-    name: "newLibDialog",
-    emits: ["hide"],
-    setup: (props, { emit }) => {
-        const dialogVisible = ref(true)
+  name: "newLibDialog",
+  emits: ["hide"],
+  setup: (props, { emit }) => {
+    const dialogVisible = ref(true);
 
-        const libform = reactive({
-            name: '',
-            path: '',
-            type: 'movie'
-        })
+    const libform = reactive({
+      name: "",
+      path: "",
+      type: "movie",
+    });
 
-        const store = useStore()
+    const store = useStore();
 
-        async function onSubmit() {
-            store.dispatch('addLib', libform).then((lib) => {
-                dialogVisible.value = false
-                emit('hide')
-                libMenuClick(lib)
-            })
-        }
-        async function onSelectDirectory() {
-            const result = await store.dispatch('invokeMainAction', {
-                action: 'showOpenDialog',
-                options: {
-                    title: '选择文件夹',
-                    properties: ['openDirectory']
-                }
-            })
-            libform.path = result.filePaths[0]
-        }
-        return {
-            libform,
-            onSubmit,
-            onSelectDirectory,
-            dialogVisible
-        }
+    async function onSubmit() {
+      store.dispatch("addLib", libform).then((lib) => {
+        dialogVisible.value = false;
+        emit("hide");
+        libMenuClick(lib);
+      });
     }
-})
+    async function onSelectDirectory() {
+      const result = await store.dispatch("invokeMainAction", {
+        action: "showOpenDialog",
+        options: {
+          title: "选择文件夹",
+          properties: ["openDirectory"],
+        },
+      });
+      libform.path = result.filePaths[0];
+    }
+    return {
+      libform,
+      onSubmit,
+      onSelectDirectory,
+      dialogVisible,
+    };
+  },
+});
 </script>
